31e8f8768446209572754ea41b813d345b82c49d
Dave Maurakis <dave@indusblue.com>
2012-09-03 17:33:03 -0400

    almost done

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,13 @@
+$(function () {
+  $(".slide .learn-more").on('click', function () {
+    alert("LEARN MORE")
+  })
 
+  $("#controls li").on('click', function (e) {
+    var slideNo = $(this).data("slide")
+    $("#controls li").removeClass("active")
+    $(this).addClass("active")
+    $(".slide").animate({ opacity : 0, "z-index" : 1 })
+    $("#slide-" + slideNo).animate({ opacity : 1, "z-index" : 2 })
+  })
+})
